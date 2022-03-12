f49a7600b7b66d51b058ee53d4e6ec625bad58b4
Amit Chandrakar <amitchandrakar028@gmail.com>
2022-03-12 14:12:07 +0530

    Feat: new apps added

--- scripts/brew_apps.sh
+++ scripts/brew_apps.sh
@@ -2,30 +2,50 @@
 
 ## MAC APPS
 # shellcheck disable=SC2039
-brew install --cask visual-studio-code
-brew install --cask google-chrome
-brew install --cask slack
-brew install --cask iterm2
-brew install --cask zoom
+
+# Utility apps
 brew install --cask vlc
-brew install --cask hyperswitch
-brew install --cask sublime-text
-brew install --cask anydesk
-brew install --cask skype
-brew install --cask wpsoffice
-brew install --cask notion
+brew install --cask google-chrome
 brew install --cask firefox
-brew install --cask lastpass
+brew install --cask iterm2
+brew install --cask hyperswitch
 brew install --cask insomnia
 brew install --cask spectacle
+brew install --cask wpsoffice
+brew install --cask alfred
+
+
+# Meeting apps & Remote support & Communication platforms
+brew install --cask anydesk
+brew install --cask skype
+brew install --cask zoom
+brew cask install teamviewer
+brew install --cask slack
 brew install --cask whatsapp
-# brew install --cask free-download-manager
-# brew install --cask alfred
-# brew install --cask evernote
-#brew cask install teamviewer
-#brew cask install upwork
+
+
+# Development apps
 #brew cask install phpstorm
+brew install --cask visual-studio-code
+brew install --cask sublime-text
+# Github desktop
+brew install --cask github
+brew install --cask postman
+brew install --cask sequel-pro
+brew install --cask lastpass
+
+
+# Workspace apps
+brew install --cask notion
+
+
+# Extra apps
+# brew install --cask free-download-manager
+# brew install --cask evernote
+#brew cask install upwork
 #brew cask install zeplin
+# brew install awscli
+
 
 ## CLI TOOLS
 brew install git
@@ -35,8 +55,6 @@ brew install composer
 brew install mysql
 wget https://www.araelium.com/querious/downloads/versions/Querious2.1.17.zip
 brew install mas
-# brew install awscli
 
 #Install Oh-MySZH (https://ohmyz.sh/#install)
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
