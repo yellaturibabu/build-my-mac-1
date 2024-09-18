e8041c055f08cb74d812d5b4dabd2a4494ac2be7
yellaturibabu <yellaturibabu@gmail.com>
2024-09-18 13:51:17 -0500

    Update brew_apps.sh

--- scripts/brew_apps.sh
+++ scripts/brew_apps.sh
@@ -11,43 +11,61 @@ brew install --cask hyperswitch
 brew install --cask insomnia
 brew install --cask spectacle
 brew install --cask alfred
-
+brew install --cask multipass
+brew install --cask notion
+brew install --cask lastpass
+brew install --cask postman
+brew install --cask dbeaver
+brew install --cask wireshark
+brew install --cask httpie
+brew install --cask hyper
+brew install --cask brave-browser
+brew install --cask mongodb-compass
+brew install --cask studio-3t
+brew install --cask cursor
+brew install --cask github
+brew install --cask obsidian
+brew install --cask todoist
+brew install --cask slack
+brew install --cask drawio
+brew install --cask zoom
+brew install --cask snagit
+brew install --cask pgadmin4
+brew install --cask microsoft-teams
+brew install --cask microsoft-onenote
+brew install --cask pycharm-ce
+brew install --cask lens
+brew install --cask docker
+brew install --cask microsoft-word
+brew install --cask microsoft-outlook
+brew install --cask day-one
+brew install --cask ollama
 
 # Meeting apps & Remote support & Communication platforms
 brew install --cask slack
 
-
-
 # Development apps
-#brew cask install phpstorm
 brew install --cask visual-studio-code
 brew install --cask sublime-text
-# Github desktop
 brew install --cask github
 brew install --cask postman
 brew install --cask lastpass
 
-
 # Workspace apps
 brew install --cask notion
 
-
 # Extra apps
-# brew install --cask free-download-manager
-# brew install --cask evernote
-#brew cask install upwork
-#brew cask install zeplin
 brew install awscli
-
+brew install azure-cli
+brew install helm
+brew install kubectl
 
 ## CLI TOOLS
 brew install git
 brew install wget
 brew install curl
-brew install composer
-# brew install mysql
-wget https://www.araelium.com/querious/downloads/versions/Querious2.1.17.zip
 brew install mas
 
-#Install Oh-MySZH (https://ohmyz.sh/#install)
+
+# Install Oh-MyZSH (https://ohmyz.sh/#install)
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
