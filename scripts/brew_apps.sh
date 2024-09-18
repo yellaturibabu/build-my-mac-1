b144e3ae7b9c73db56e52046dec591e34b6d9ab9
yellaturibabu <yellaturibabu@gmail.com>
2024-09-18 13:45:37 -0500

    Update brew_apps.sh

--- scripts/brew_apps.sh
+++ scripts/brew_apps.sh
@@ -4,24 +4,18 @@
 # shellcheck disable=SC2039
 
 # Utility apps
-brew install --cask vlc
 brew install --cask google-chrome
 brew install --cask firefox
 brew install --cask iterm2
 brew install --cask hyperswitch
 brew install --cask insomnia
 brew install --cask spectacle
-brew install --cask wpsoffice
 brew install --cask alfred
 
 
 # Meeting apps & Remote support & Communication platforms
-brew install --cask anydesk
-brew install --cask skype
-brew install --cask zoom
-brew cask install teamviewer
 brew install --cask slack
-brew install --cask whatsapp
+
 
 
 # Development apps
@@ -31,7 +25,6 @@ brew install --cask sublime-text
 # Github desktop
 brew install --cask github
 brew install --cask postman
-brew install --cask sequel-pro
 brew install --cask lastpass
 
 
@@ -44,7 +37,7 @@ brew install --cask notion
 # brew install --cask evernote
 #brew cask install upwork
 #brew cask install zeplin
-# brew install awscli
+brew install awscli
 
 
 ## CLI TOOLS
@@ -52,7 +45,7 @@ brew install git
 brew install wget
 brew install curl
 brew install composer
-brew install mysql
+# brew install mysql
 wget https://www.araelium.com/querious/downloads/versions/Querious2.1.17.zip
 brew install mas
 
